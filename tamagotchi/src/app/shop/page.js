800fa2b7905a0e296776c329e245217ed586f0ca
Darin Cheng <darincheng2026@u.northwestern.edu>
2024-11-28 15:07:35 -0600

    add stub for shop page

--- tamagotchi/src/app/shop/page.js
+++ tamagotchi/src/app/shop/page.js
@@ -1,5 +1,22 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+
 export default function Shop() {
-    return (<>
-        <h1>Shop</h1>
-    </>);
+  let items;
+  let setItems;
+  [items, setItems] = useState([1, 2, 3, 4]);
+
+  return (
+    <div className="h-screen flex flex-col justify-center items-center">
+      <h1>Shop</h1>
+      <div className="grid">
+        {items.map((item, idx) => (
+          <h1 key={idx}>{item}</h1>
+        ))}
+      </div>
+      <Link href="/">Return</Link>
+    </div>
+  );
 }
