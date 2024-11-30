1fe262c47df551fb6e762ea4db00872b00f9b301
Darin Cheng <darincheng2026@u.northwestern.edu>
2024-11-30 14:57:21 -0600

    fix formatting

--- tamagotchi/src/components/Button.js
+++ tamagotchi/src/components/Button.js
@@ -1,10 +1,10 @@
-export default function Button({text, onClick}) {
-    return (
-      <button
-        className="px-4 py-2 bg-red-400 text-white rounded"
-        onClick={onClick}
-      >
-        {text}
-      </button>
-    );
+export default function Button({ text, onClick }) {
+  return (
+    <button
+      className="px-4 py-2 bg-red-400 text-white rounded"
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
 }
--- tamagotchi/src/components/Shop.js
+++ tamagotchi/src/components/Shop.js
@@ -3,17 +3,20 @@ import ShopItem from "./ShopItem";
 import { useState } from "react";
 
 export default function Shop() {
-    const [isShopOpen, setIsShopOpen] = useState(false);
-    function openShop() {
-        setIsShopOpen(!isShopOpen);
-    }
-    return (<>
-        <Button text="Shop" onClick={openShop} />
-        {isShopOpen && (
-            <div className="flex flex-col md:flex-row gap-4">
-                <ShopItem name="Puffle Chow" icon="🥫" price="10" />
-                <ShopItem name="Medicine" icon="💊" price="10" />
-                <ShopItem name="Toy" icon="🪀" price="10" />
-            </div>)}
-    </>);
+  const [isShopOpen, setIsShopOpen] = useState(false);
+  function openShop() {
+    setIsShopOpen(!isShopOpen);
+  }
+  return (
+    <>
+      <Button text="Shop" onClick={openShop} />
+      {isShopOpen && (
+        <div className="flex flex-col md:flex-row gap-4">
+          <ShopItem name="Puffle Chow" icon="🥫" price="10" />
+          <ShopItem name="Medicine" icon="💊" price="10" />
+          <ShopItem name="Toy" icon="🪀" price="10" />
+        </div>
+      )}
+    </>
+  );
 }
