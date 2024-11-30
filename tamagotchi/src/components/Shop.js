3ab9070d9becae0ac7f1fcfbf639981a2391e44b
Darin Cheng <darincheng2026@u.northwestern.edu>
2024-11-30 14:57:06 -0600

    add items to shop and organize in container

--- tamagotchi/src/components/Shop.js
+++ tamagotchi/src/components/Shop.js
@@ -1,5 +1,5 @@
 import Button from "./Button";
-import Item from "./Item";
+import ShopItem from "./ShopItem";
 import { useState } from "react";
 
 export default function Shop() {
@@ -10,7 +10,10 @@ export default function Shop() {
     return (<>
         <Button text="Shop" onClick={openShop} />
         {isShopOpen && (
-        )
-        }
+            <div className="flex flex-col md:flex-row gap-4">
+                <ShopItem name="Puffle Chow" icon="🥫" price="10" />
+                <ShopItem name="Medicine" icon="💊" price="10" />
+                <ShopItem name="Toy" icon="🪀" price="10" />
+            </div>)}
     </>);
 }
