283499f813e1e44c5cf93c60c70c5523c2b7664e
Darin Cheng <darincheng2026@u.northwestern.edu>
2024-11-30 16:11:14 -0600

    items will change values of hunger, health, and happiness

--- tamagotchi/src/components/Inventory.js
+++ tamagotchi/src/components/Inventory.js
@@ -1,12 +1,19 @@
 import InventoryItem from "./InventoryItem";
 
-export default function Inventory({ inventory, setInventory }) {
+export default function Inventory({ inventory, setInventory, barStates }) {
   function useItem(icon) {
     const newInventory = { ...inventory };
     if (newInventory[icon] === 1) {
       delete newInventory[icon];
     } else {
       newInventory[icon] -= 1;
+    }
+    if (icon === "🥫") {
+        barStates.hunger.setState(Math.min(100, barStates.hunger.state + 30));
+    } else if (icon === "💊") {
+        barStates.health.setState(Math.min(100, barStates.health.state + 30));
+    } else if (icon === "🪀") {
+        barStates.happiness.setState(Math.min(100, barStates.happiness.state + 30));
     }
     setInventory(newInventory);
   }
